f071dae533f39096601204f9ca98667d1c5467e0
thiagocantero@gmail.com <thiagocantero@gmail.com>
2022-09-16 15:17:56 -0300

    first commit

---
+++ resources/lang/en/datatables.php
@@ -0,0 +1,25 @@
+<?php
+return [
+    "filter" => "Filtro",
+    "sEmptyTable" => "Não há dados para exibir",
+    "sInfo" => "Showing _START_ to _END_ of _TOTAL_ entries",
+    "sInfoEmpty" => "Demonstrando 0 to 0 de 0 registros",
+    "sInfoFiltered" => "(filtered from _MAX_ total entries)",
+    "sInfoPostFix" => "",
+    "sInfoThousands" => ",",
+    "sLengthMenu" => "Show _MENU_ entries",
+    "sLoadingRecords" => "Carregando...",
+    "sProcessing" => "Processando...",
+    "sSearch" => "Pesquisar:",
+    "sZeroRecords" => "Nenhum registro encontrado",
+    "oPaginate" => [
+        "sFirst" => "Primeiro",
+        "sLast" => "Último",
+        "sNext" => "Próximo",
+        "sPrevious" => "Anterior"
+    ],
+    "oAria" => [
+        "sSortAscending" => " => activate to sort column ascending",
+        "sSortDescending" => " => activate to sort column descending"
+    ]
+];
